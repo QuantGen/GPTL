04b2baa9708a182bfa32450966dc22ce5da9e81e
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-04 15:55:50 -0400

    Update sample_effects.c

--- src/sample_effects.c
+++ src/sample_effects.c
@@ -68,7 +68,6 @@ SEXP sample_effects(SEXP C, SEXP rhs, SEXP b, SEXP d, SEXP B0,SEXP varE, SEXP va
        pb[j]=sol;//+z;
     }
     // Creating a list to return results
-
     PutRNGstate();
       
     UNPROTECT(6);
