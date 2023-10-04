234208be93250f10ca29146a287f1fc6d1249744
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-04 13:20:26 -0400

    Create init.c

---
+++ src/init.c
@@ -0,0 +1,17 @@
+#include "fitLSYS.h"
+
+#include "GRAD_DESC.h"
+
+#include <R_ext/Rdynload.h>
+
+static const R_CallMethodDef callMethods[] = {
+    {"fitLSYS", (DL_FUNC) fitLSYS, 7},
+    {"GRAD_DESC", (DL_FUNC) GRAD_DESC, 6},
+    {NULL, NULL, 0}
+};
+
+void R_init_BGDataExt(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, callMethods, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+    R_forceSymbols(dll, TRUE);
+}
