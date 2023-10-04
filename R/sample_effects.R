357f0288b8231bd179549288390994925c2bfd1c
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-04 15:01:22 -0400

    Update sample_effects.R

--- R/sample_effects.R
+++ R/sample_effects.R
@@ -1,5 +1,4 @@
-sample_effects=function(C,rhs,b,d,B0,varE,varB){
-  
+sample_effects=function(C,rhs,b,d,B0,varE,varB){  
      d<- d - 1L # for the 0-based index
      ans <- .Call("sample_effects", C,rhs,b,d,B0,varE,varB)
      return(ans)
