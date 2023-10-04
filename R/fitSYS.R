c030ab4be04fe8163a032c7b03fd83757273a273
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-04 13:31:25 -0400

    Update fitSYS.R

--- R/fitSYS.R
+++ R/fitSYS.R
@@ -1,6 +1,6 @@
 
 # A wrappper to a function that solves a system of linear equations using Gauss Seidel
-fitSYS <- function(C, rhs, b, active, RSS, maxIter, tol) {
+fitLSYS <- function(C, rhs, b, active, RSS, maxIter, tol) {
     active <- active - 1L # for the 0-based index
     ans <- .Call("fitLSYS", C, rhs, b, active, RSS, maxIter, tol)
     return(list(b = ans[[1]], RSS = ans[[2]]))
