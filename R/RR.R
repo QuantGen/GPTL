cd30fae8c4a61a8c99736e246b227de6c0dc0ac7
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-05 09:23:16 -0400

    Update RR.R

--- R/RR.R
+++ R/RR.R
@@ -1,7 +1,7 @@
 # A wrappper for ridge regression, possibly shrinking towards b0
 # For now we just have ridge regression.. we will extend to LASSOand Elastic Net
  RR <- function(XX, Xy,lambda,p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda0=0, active=1:p, RSS=10000, maxIter=1000, tol=1e-5) {
-     
+     # Loss function is (y-Xb)'(y-Xb)+ lambda|b-lambda0*b0 |^2
     # adding the shrinkage parameter to the left-hand-side
     diag(XX)=diag(XX)+lambda
 
