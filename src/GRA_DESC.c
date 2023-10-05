e586d8e3331433b17504784d174f7185f14d1cbf
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-05 19:27:13 -0400

    Update GRA_DESC.c

--- src/GRA_DESC.c
+++ src/GRA_DESC.c
@@ -1,35 +1,29 @@
 #include "GRAD_DESC.h"
 
-SEXP GRAD_DESC(SEXP C, SEXP rhs, SEXP b, SEXP active, SEXP nIter, SEXP learning_rate) {
+SEXP GRAD_DESC(SEXP C, SEXP rhs, SEXP b, SEXP nIter, SEXP learning_rate) {
 
     int p = Rf_ncols(C);
-    R_xlen_t nActive = Rf_xlength(active);
     int n_iter = Rf_asInteger(nIter);
     double LR =Rf_asReal(learning_rate);
     double *pC = REAL(C);
     double *prhs = REAL(rhs);
     b = PROTECT(Rf_duplicate(b));
     double *pb = REAL(b);
-    int *pactive = INTEGER(active);
-
-    for (int iter = 0; iter < n_iter; iter++) {
-        
-        for (int j = 0; j < nActive; j++) { // loop over active predictors
-            int k = pactive[j];
-            //double Ckk = pC[k * (p + 1)];
-            double offset = 0;      
-            
-            for (int m = 0; m < nActive; m++) {
-                int n = pactive[m];
-                offset += pC[p * k + n] * pb[n];
-            }  
-        
-            double gradient=-(prhs[k]-offset);//+2*Ckk*pb[k]
-            double sol=pb[k]-LR*gradient ;
+    int inc=1;
+    double offset ; 
+    double Cjj;
+    double gradient;
+    double sol;
+    
+    for (int iter = 0; iter < n_iter; iter++) {       
+        for (int j = 0; j < p; j++) { // loop over predictors
+            Cjj = pC[j * (p + 1)]; 
+            offset=F77_NAME(ddot)(&p, pXX+j_global*p, &inc, pbeta, &inc);
+            gradient=2*Ckk*pb[j] - (prhs[j]-offset);
+            sol=pb[j]-LR*gradient ;
             pb[k] = sol;
         }
     }
-
 
     // Creating a list to return results
     SEXP list = PROTECT(Rf_allocVector(VECSXP, 1));
