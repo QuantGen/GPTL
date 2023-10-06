149d57f54272740f86d8a99a1122679305efc48a
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-05 20:32:59 -0400

    Update PSS.R

--- R/PSS.R
+++ R/PSS.R
@@ -1,12 +1,16 @@
 ## This function simply pools summary statistics using weights, the output can be use as inputs in both BLRCross(), RR(), or GD()
 
-
+if(FALSE){
 PSS=function(SSList,weights){
   # SSList should be a list of sufficient statistics with the ith element having [[i]]$XX, [[i]]$Xy, [[i]]$my, [[i]]$vy, [[i]]$n
   # weights: a vecctor of weights of the same length as SSList
   # Value: a list with the pooled sufficient staitistics XX, Xy, my, vy, and the estimated effective sample size (see https://www.jepusto.com/effective-sample-size-aggregation/)
+
   weights=weights/sum(weightS)
-  if(any(weights<=0)){ stop('All weights must be greater than 0') }
+  
+  if(any(weights<=0)){ 
+    stop('All weights must be greater than 0') 
+  }
 
   p1=ncol(SSList[[1]]$XX)
   p2=length(SSList[[1]]$Xy)
@@ -14,4 +18,5 @@ PSS=function(SSList,weights){
   # Complete code here
 
   #return(list(XX=XX,Xy=Xy,my=my,vy=vy,ESS=ESS)) # ESS is the effective sample size
+}
 }
