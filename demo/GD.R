94cf3bc846f6a9aa1efcf848c020097634a0e474
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-05 21:40:00 -0400

    Update GD.R

--- demo/GD.R
+++ demo/GD.R
@@ -1,1 +1,45 @@
-D
+#library(remotes)
+#install_github('https://github.com/quantGen/GPTL')
+
+
+library(GPTL)
+
+
+ rm(list=ls())
+
+ ## Simple OLS problem
+
+ X=matrix(nrow=100,ncol=5,rnorm(500))
+ b=rnorm(5)*2
+ signal=X%*%b
+ y=signal+rnorm(nrow(X),sd=sd(signal))
+
+ bHat1=coef(lm(y~X))
+
+ XX=crossprod(cbind(1,X),cbind(1,X))
+ Xy=crossprod(cbind(1,X),y)
+
+ bHat2=GD(XX,Xy,lambda=0,nIter=100,learning_rate=.1);plot(bHat1,bHat2,cex=1.5,col=2);abline(a=0,b=1)
+
+
+## Ridge Regression problem
+
+
+
+ library(BGLR)
+ data(wheat)
+
+ X=scale(wheat.X,center=TRUE,scale=FALSE)
+ y=wheat.Y[,1]
+
+ XX=crossprod(X)
+ Xy=crossprod(X,y)
+ n=nrow(X)
+
+ lambda=sum(diag(XX))/n
+
+
+ bHat=RR(XX,Xy,lambda=lambda)
+ bHat2=GD(XX,Xy,lambda=lambda,nIter=100,learning_rate=.1);plot(bHat,bHat2);abline(a=0,b=1)
+
+
