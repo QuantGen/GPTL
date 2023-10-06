520fd85333ccf1a219803c29e6404f32812b7438
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-05 20:29:03 -0400

    Update GRA_DESC.c

--- src/GRA_DESC.c
+++ src/GRA_DESC.c
@@ -41,7 +41,7 @@ SEXP GRAD_DESC(SEXP C, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP learning_ra
         for (j = 0; j < p; j++) { // loop over predictors
             Cjj = pC[j * (p + 1)]; 
             offset=0;//F77_NAME(ddot)(&p, pC+j*p, &inc, pb, &inc);
-            gradient=2*Ckk*pb[j] - (prhs[j]-offset);
+            gradient=2*Cjj*pb[j] - (prhs[j]-offset);
             sol=pb[j]-LR*gradient ;
             pb[j] = sol;
         }
