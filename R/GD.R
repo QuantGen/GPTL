74fda1446cfcd38551deda7a7864ce7469eb41c1
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-05 20:47:48 -0400

    Update GD.R

--- R/GD.R
+++ R/GD.R
@@ -1,7 +1,7 @@
 
 # A wrapper to the C-function that performs Gradient Descent in a system of linear equations
 
-GD<- function(XX,Xy,p=ncol(XX),b=rep(0,p), nIter=10,learning_rate=1/5,lambda=0,b0=rep(0,p),lambda0=1,returnPath=FALSE){
+GD<- function(XX,Xy,p=ncol(XX),b=rep(0,p), nIter=10,learning_rate=1/10,lambda=0,b0=rep(0,p),lambda0=1,returnPath=FALSE){
     
     diag(XX)=diag(XX)+lambda
     learning_rate=learning_rate/mean(diag(XX))
@@ -10,9 +10,8 @@ GD<- function(XX,Xy,p=ncol(XX),b=rep(0,p), nIter=10,learning_rate=1/5,lambda=0,b
      B=matrix(nrow=ncol(XX),ncol=nIter+1,NA)
      B[,1]=b
      for(i in 2:ncol(B)){
-
         #GRAD_DESC(SEXP C, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP learning_rate)
-        B[,i]=.Call("GRAD_DESC",XX, Xy, B[,i-1],p, 1, learning_rate)[[1]]
+        B[,i]=.Call("GRAD_DESC",XX, Xy, B[,i-1],p, 1, learning_rate)
      }
      rownames(B)=rownames(XX)
      colnames(B)=paste0('iter_',0:(nIter-1))
