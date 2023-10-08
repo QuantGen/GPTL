3e6038b070d889377032b812b669234da4474cbf
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-08 10:16:50 -0400

    Update sample_multinomial.c

--- src/sample_multinomial.c
+++ src/sample_multinomial.c
@@ -25,7 +25,7 @@ SEXP rMultinomial(SEXP PROB, SEXP n, SEXP p) {
     PROTECT(prob=NEW_NUMERIC(nCat));
     p_prob=NUMERIC_POINTER(prob);
 
-    PRODECT(cumProb=NEW_NUMERIC(nCat));
+    PROTECT(cumProb=NEW_NUMERIC(nCat));
     p_cumProb=NUMERIC_POINTER(p_cumProb);
 
     PROTECT(samples = NEW_INTEGER(nCat)); 
