75b8260ac70d7e4983a12a26bb1b4b261c0693af
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-08 19:20:53 -0400

    Update GRA_DESC.c

--- src/GRA_DESC.c
+++ src/GRA_DESC.c
@@ -30,11 +30,11 @@ SEXP GRAD_DESC(SEXP C, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP learning_ra
     
     for (int iter = 0; iter < niter; iter++) {       
         for (j = 0; j < p; j++) { // loop over predictors
-            Cjj = pC[j * (p + 1)]; 
+            Cjj = pC[j * (p + 1)]; // I was using this for LR but it gave me numerical problems
             Cjb=F77_NAME(ddot)(&p, pC+j*p, &inc, pb, &inc); //C[,j]'b
             gradient= - (prhs[j]-Cjb);   
             // dividing by Cjj (the 2nd derivative) makes step smaller when the function has more curvature
-            pb[j]-=LR*gradient/Cjj ; 
+            pb[j]-=LR*gradient ; 
         }
     }
     
