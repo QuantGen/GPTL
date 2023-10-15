ef25374483d5d6e33c686826ce0a2d3a54e94d05
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-15 15:34:33 -0400

    Update GD.R

--- R/GD.R
+++ R/GD.R
@@ -8,7 +8,8 @@ GD<- function(XX,Xy,p=ncol(XX),b=rep(0,p), nIter=10,learning_rate=1/10,lambda=0,
     if(lambda>0 & lambda0>0 ){    Xy=Xy+lambda*lambda0*b0 }
 
     learning_rate=learning_rate/mean(diag(XX))
-    
+
+    if(returnPath){
      B=matrix(nrow=ncol(XX),ncol=nIter+1,NA)
      B[,1]=b
      for(i in 2:ncol(B)){
@@ -16,8 +17,8 @@ GD<- function(XX,Xy,p=ncol(XX),b=rep(0,p), nIter=10,learning_rate=1/10,lambda=0,
      }
      rownames(B)=rownames(XX)
      colnames(B)=paste0('iter_',0:(nIter))
-    if(!returnPath){ 
-        B=B[,ncol(B)] 
+    }else{ 
+        B=.Call("GRAD_DESC",XX, Xy, B[,i-1],p, nIter, learning_rate)
     }
     return(B)
 }
