9330881476b3617c2f50ba6192b434e0e78a6466
Harold Wu <wuhao12@msu.edu>
2023-10-18 10:53:49 -0400

    Update GD.R

--- R/GD.R
+++ R/GD.R
@@ -3,6 +3,8 @@
 
 GD<- function(XX,Xy,p=ncol(XX),b=rep(0,p), nIter=10,learning_rate=1/10,lambda=0,b0=rep(0,p),
               lambda0=1,returnPath=FALSE){
+
+    
     if(lambda>0){   diag(XX)=diag(XX)+lambda }
 
     if(lambda>0 & lambda0>0 ){    Xy=Xy+lambda*lambda0*b0 }
@@ -18,7 +20,7 @@ GD<- function(XX,Xy,p=ncol(XX),b=rep(0,p), nIter=10,learning_rate=1/10,lambda=0,
      rownames(B)=rownames(XX)
      colnames(B)=paste0('iter_',0:(nIter))
     }else{ 
-        B=.Call("GRAD_DESC",XX, Xy, b,p, nIter, learning_rate)
+        B=.Call("GRAD_DESC",XX, Xy, b+0,p, nIter, learning_rate)
     }
     #if(!returnPath){
     #  B=B[,ncol(B),drop=TRUE]
