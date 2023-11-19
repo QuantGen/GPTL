0162074f584b0099496d4ea3d18c0d6dd24cc381
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-11-19 18:01:24 -0500

    Update GD.R

--- R/GD.R
+++ R/GD.R
@@ -45,9 +45,9 @@ GD2<- function( XX,Xy,p=ncol(XX),b=rep(0,p), nIter=10,learning_rate=1/10,
 
         if(returnPath){
             B=matrix(nrow=ncol(XX),ncol=nIter+1,NA)
-            B[,1]=b
+            B[,1,h]=b
             for(i in 2:ncol(B)){
-              B[,,h]=.Call("GRAD_DESC",XX, Xy, B[,i-1],p, 1, LR)
+              B[,i,h]=.Call("GRAD_DESC",XX, Xy, B[,i-1,h],p, 1, LR)
             }
         }else{ 
             B[,1,h]=.Call("GRAD_DESC",XX, Xy, b+0,p, nIter, LR)
