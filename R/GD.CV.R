18a9c9cf6176c8036a39ae34d98f39e8c3cddea7
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-11-24 09:34:46 -0500

    Create GD.CV.R

---
+++ R/GD.CV.R
@@ -0,0 +1,40 @@
+
+GD.CV=function(X,y,nFolds=5,nRep=10,acc=cor,suppress_warnings=TRUE,...){
+    
+    DIM=c(nIter,length(lambda),nRep)
+    ACC=array(dim=DIM)
+
+    N=nrow(X)
+    for(i in 1:nRep){
+        folds=sample(1:nFolds,size=N,replace=TRUE)
+        AVG_ACC=array(dim=c(nIter,length(lambda)),0)
+
+        for(j in 1:nFolds){
+            tst=which(folds==j)
+
+            XTRN=X[-tst,,drop=FALSE]
+            yTRN=y[-tst]
+            XX=crossprod(XTRN)
+            Xy=crossprod(XTRN,yTRN)
+            #B=GD(XX=XX,Xy=Xy,...)
+            B=GD(XX=XX,Xy=Xy,lambda=lambda,nIter=nIter,returnPath=returnPath)
+            XTST=X[tst,,drop=FALSE]
+            yTST=y[tst]
+
+            for(Row in 1:nrow(AVG_ACC)){
+                for(Col in 1:ncol(AVG_ACC)){
+                    if(suppress_warnings){
+                        suppressWarnings( AVG_ACC[Row,Col]<-AVG_ACC[Row,Col]+acc(yTST,XTST%*%B[,Row,Col]) )
+                    }        
+                }
+            }
+            
+        }
+        AVG_ACC=AVG_ACC/nFolds
+        ACC[,,i]=AVG_ACC
+    }
+
+    TMP=list(ACC=apply(FUN=mean,MARGIN=c(1,2),X=ACC),SD=apply(FUN=sd,MARGIN=c(1,2),X=ACC))
+    return(TMP)
+
+}
