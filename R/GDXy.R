8d8c9bbde587831599dbd17fb3a03f8c6d3811d2
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-11-24 09:10:37 -0500

    Create GDXy.R

---
+++ R/GDXy.R
@@ -0,0 +1,14 @@
+# A function to perform Grad. Desc. that takes X and y, instead of XX and Xy
+
+GDXy=function(X,y,centerX=TRUE,scaleX=FALSE,...){
+
+    X=scale(X,center=centerX,scale=scaleX)
+    y=scale(y,center=TRUE)
+
+    XX=crossprod(X)
+    Xy=crossprod(X,y)
+
+    B=GD(XX=XX,Xy=Xy,...)
+
+    return(B)
+}
