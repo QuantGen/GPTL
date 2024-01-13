b8b9f6d15929436b584ed90b988d9983b0a74e23
Gustavo de los Campos <gdeloscampos@gmail.com>
2024-01-13 07:15:29 -0500

    Update GDXy.R

--- R/GDXy.R
+++ R/GDXy.R
@@ -1,6 +1,6 @@
 # A function to perform Grad. Desc. that takes X and y, instead of XX and Xy
 
-GDXy=function(X,y,centerX=TRUE,scaleX=FALSE,...){
+GDXy=function(X,y,centerX=TRUE,scaleX=FALSE,sortGradient=FALSE,...){
 
     X=scale(X,center=centerX,scale=scaleX)
     y=scale(y,center=TRUE)
