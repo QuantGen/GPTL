37678453c668295fadf7c0838c3d1703ffccf141
Harold Wu <wuhao12@msu.edu>
2024-01-30 10:59:58 -0500

    Update RIDGE_CD.R

--- R/RIDGE_CD.R
+++ R/RIDGE_CD.R
@@ -2,20 +2,6 @@
 
 # C code implemented
 RIDGE.CD<- function(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=NULL,nIter=100,returnPath=TRUE) {
-
-  ## Notes
-  #  To match glment, XX and Xy should be crossprod(X)/n and crossprod(X,y)/n
-  #  With both X and y centered
-  ##
-
-  ## Default lambda for LASSO
-  if(is.null(lambda)){
-    bOLS=Xy/diag(XX)
-    lambda.max=max(diag(XX)*abs(bOLS-b0))+1e-4
-    lambda.min=lambda.max/100
-    lambda=exp(seq(from=log(lambda.max),to=log(lambda.min),length=10))# glmnet uses length=100
-  }
-
   
   B=array(dim=c(p,nIter,length(lambda)))
   bIni=b
@@ -24,7 +10,7 @@ RIDGE.CD<- function(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=NULL,nIter
  
     B[,1,h]=bIni
     for (i in 2:nIter) {
-      B[,i,h]=.Call("LASSO_CD",XX, Xy, B[,i-1,h],p, 1, lambda[h], b0)
+      B[,i,h]=.Call("RIDGE_CD",XX, Xy, B[,i-1,h],p, 1, lambda[h], b0)
     }
   }
 
