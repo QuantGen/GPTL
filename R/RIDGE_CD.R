2b02ca88395c9f952c351dbbfd0291e71d8bced2
Harold Wu <wuhao12@msu.edu>
2024-01-30 11:11:03 -0500

    Update RIDGE_CD.R

--- R/RIDGE_CD.R
+++ R/RIDGE_CD.R
@@ -22,3 +22,24 @@ RIDGE.CD<- function(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=NULL,nIter
     return(B[,nIter,,drop=TRUE])
   }
 }
+
+if(FALSE){
+  n=1000
+  p=10
+  QTL=c(2,4,6,8)
+  X=matrix(nrow=n,ncol=p,rnorm(n*p))
+  b=rep(0,p)
+  b[QTL]=1
+  signal=X%*%b
+  error=rnorm(sd=sd(signal),n=n)
+  y=signal+error
+
+  XX=crossprod(X)
+  Xy=crossprod(X,y)
+
+  fm_CD=RIDGE.CD(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=10,nIter=50,returnPath=FALSE)
+  #compare with Gradient Descent
+  fm_GD=GD(XX,Xy,p=ncol(XX),b=rep(0,p), nIter=50,learning_rate=1/10,
+               lambda=10,b0=rep(0,p),lambda0=1,returnPath=FALSE,sortGradient=FALSE)
+
+}
