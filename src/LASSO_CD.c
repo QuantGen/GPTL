482b7f61a4356d385d43e3202b3815516275245f
Harold Wu <wuhao12@msu.edu>
2024-01-30 10:36:30 -0500

    Update LASSO_CD.c

--- src/LASSO_CD.c
+++ src/LASSO_CD.c
@@ -39,16 +39,16 @@ SEXP LASSO_CD(SEXP C, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP lambda, SEXP
             offset=Cjb-Cjj*pb[j];
             bOLS=(prhs[j]-offset)/Cjj;
             // sign function
-            if (bOLS < 0) {
+            if (bOLS-pb0[j] < 0) {
                 sign_bOLS=-1;
-            } else if (bOLS == 0) {
+            } else if (bOLS-pb0[j] == 0) {
                 sign_bOLS=0;
             } else {
                 sign_bOLS=1;
             }
             
-            if (abs(bOLS - pb0[j]) > Lambda) {
-                pb[j]=bOLS-sign_bOLS*Lambda;
+            if (abs(bOLS - pb0[j]) > Lambda/Cjj) {
+                pb[j]=bOLS-sign_bOLS*Lambda/Cjj;
             } else {
                 pb[j]=pb0[j];
             }
