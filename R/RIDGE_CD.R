a1d527de1f0d709f032a964a19d832b785c6b35b
Harold Wu <wuhao12@msu.edu>
2024-01-31 10:08:16 -0500

    Update RIDGE_CD.R

--- R/RIDGE_CD.R
+++ R/RIDGE_CD.R
@@ -2,6 +2,13 @@
 
 # C code implemented
 RIDGE.CD<- function(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=NULL,nIter=100,returnPath=TRUE) {
+
+  if(is.null(lambda)){
+    bOLS=Xy/diag(XX)
+    lambda.max=max(diag(XX)*abs(bOLS-b0))+1e-4
+    lambda.min=lambda.max/100
+    lambda=exp(seq(from=log(lambda.max),to=log(lambda.min),length=10))# glmnet uses length=100
+  }
   
   B=array(dim=c(p,nIter,length(lambda)))
   bIni=b
