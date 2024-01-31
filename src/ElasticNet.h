9885b4d3c6795d48770cc72b73e7aa82cd440bbb
Harold Wu <wuhao12@msu.edu>
2024-01-31 10:44:22 -0500

    Update ElasticNet.h

--- src/ElasticNet.h
+++ src/ElasticNet.h
@@ -2,4 +2,4 @@
 
 #include <Rinternals.h>
 
-SEXP ElasticNet(SEXP C, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP lambda, SEXP b0, SEXP alpha);
+SEXP ElasticNet(SEXP C, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP lambda1, SEXP lambda2, SEXP b0);
