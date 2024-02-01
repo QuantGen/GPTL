540ce941c2dacd8f94f4b3576a909b6aa0d558a5
Harold Wu <wuhao12@msu.edu>
2024-02-01 09:35:52 -0500

    Update ElasticNet.R

--- R/ElasticNet.R
+++ R/ElasticNet.R
@@ -1,60 +1,33 @@
 # Elastic Net
 
 # C code implemented
-ElasticNet<- function(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=NULL,alpha=0.5,nIter=100,returnPath=TRUE) {
+ElasticNet<- function(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=NULL,nLambda=30,alpha=0.5,nIter=100,returnPath=TRUE) {
   #alpha=0 -> Ridge; alpha=1 -> Lasso
   
   if(is.null(lambda)){
-    bOLS=Xy/diag(XX)
-    lambda.max=max(diag(XX)*abs(bOLS-b0))+1e-4
-    lambda.min=lambda.max/100
-    lambda=exp(seq(from=log(lambda.max),to=log(lambda.min),length=10))# glmnet uses length=100
+    lambda.max=max(abs(Xy))+1e-4
+    lambda.min=lambda.max/30
+    lambda=seq(from=lambda.max,to=lambda.min,length=nLambda)
   }
   
-  B=array(dim=c(p,nIter,length(lambda)))
-  bIni=b
+  B=array(dim=c(p,nIter,nLambda))
+  dimnames(B)=list(colnames(XX),paste0('iter_',1:nIter),paste0('lambda_',round(lambda,2)))
+  
   lambda1=lambda*alpha
   lambda2=lambda*(1-alpha)
   
   for (h in 1:length(lambda)) {
- 
-    B[,1,h]=bIni
-    for (i in 2:nIter) {
-      B[,i,h]=.Call("ElasticNet",XX, Xy, B[,i-1,h],p, 1, lambda1[h], lambda2[h], b0)
+    bIni=b
+    for (i in 1:nIter) {
+      bIni=.Call("ElasticNet",XX, Xy, bIni, p, 1, lambda1[h], lambda2[h], b0)
+      B[,i,h]=bIni
     }
   }
 
-  iterations=paste0('iter_',1:nIter)
-  dimnames(B)=list(colnames(XX),iterations,paste0('lambda_',lambda))
   if (returnPath) {
-    return(B[,,,drop=TRUE])
+    return(list(B=B[,,,drop=TRUE], lambda=lambda, alpha=alpha))
   } else {
-    return(B[,nIter,,drop=TRUE])
+    return(list(B=B[,nIter,,drop=TRUE], lambda=lambda, alpha=alpha))
   }
 }
 
-if(FALSE){
-  n=1000
-  p=10
-  QTL=c(2,4,6,8)
-  X=matrix(nrow=n,ncol=p,rnorm(n*p))
-  b=rep(0,p)
-  b[QTL]=1
-  signal=X%*%b
-  error=rnorm(sd=sd(signal),n=n)
-  y=signal+error
-
-  XX=crossprod(X)
-  Xy=crossprod(X,y)
-
-  library(glmnet)
-  fmL=glmnet(y=y,x=X, alpha=0.5)
-  fm_EN=ElasticNet(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=fmL$lambda,nIter=50,returnPath=T, alpha=0.5)
-
-
-  par(mfrow=c(3,3))
-  for(i in seq(from=2,to=18,by=2)){
-    plot(fmL$beta[,i],fm_EN[,50,i],col=4,cex=1.5);abline(a=0,b=1)
-  }
-
-}
