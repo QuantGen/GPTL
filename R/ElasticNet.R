f4638c7ffd8ac5403b1410f542637df3b34a96a2
Harold Wu <wuhao12@msu.edu>
2024-12-09 11:25:11 -0500

    Update ElasticNet.R

--- R/ElasticNet.R
+++ R/ElasticNet.R
@@ -6,8 +6,16 @@ ElasticNet<- function(XX, Xy, p=ncol(XX), b=rep(0,p),b0=rep(0,p),lambda=NULL,nLa
   
   if(is.null(lambda)){
     lambda.max=max(abs(Xy))+1e-4
-    lambda.min=lambda.max/30
-    lambda=seq(from=lambda.max,to=lambda.min,length=nLambda)
+    lambda.min=lambda.max/10000
+    lambda.lasso=seq(from=lambda.max,to=lambda.min,length=nLambda)
+
+    h2.min=.005
+ 	  h2.max=0.995
+ 	  h2.grid=exp(seq(from=log(h2.min),to=log(h2.max),length=nLambda))
+ 	  K=mean(diag(XX))
+ 	  lambda.ridge=K*(1-h2.grid)/h2.grid
+
+    lambda=alpha*lambda.lasso+(1-alpha)*sqrt(lambda.ridge)
   }
   
   B=array(dim=c(p,maxIter,length(lambda)))
