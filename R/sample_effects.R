a8d626cc083312f000689efb2c891a6d80fce3e9
Paulino Pérez-Rodríguez <perpdgo@gmail.com>
2025-06-05 21:12:18 -0600

    Add files via upload

--- R/sample_effects.R
+++ R/sample_effects.R
@@ -9,3 +9,11 @@ sample_effects_new=function(C,rhs,b,d,B0,varE,varB,RSS){
      ans <- .Call("sample_effects_new", C,rhs,b,d,B0,varE,varB,RSS)
      return(ans)
 }
+
+sample_effects_new_sparse=function(C,rhs,b,d,B0,varE,varB,RSS)
+{
+	d<- d - 1L # for the 0-based index
+	diagC<- Matrix::diag(C)
+	ans <- .Call("sample_effects_new_sparse",C@x,C@p,C@i,diagC,ncol(C),rhs,b,d,B0,varE,varB,RSS)
+	return(ans)
+}
