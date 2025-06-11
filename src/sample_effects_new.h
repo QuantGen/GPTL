cf707c306015638e6350763bddf664200a19489e
Paulino Pérez-Rodríguez <perpdgo@gmail.com>
2025-06-11 12:15:58 -0600

    Delete src/sample_effects_new.h

--- src/sample_effects_new.h
+++
@@ -1,6 +0,0 @@
-#define R_NO_REMAP
-
-#include <Rinternals.h>
-
-SEXP sample_effects_new(SEXP C, SEXP rhs, SEXP b, SEXP d, SEXP B0,SEXP varE, SEXP varB, SEXP RSS);
-SEXP sample_effects_new_sparse(SEXP C, SEXP cOffset, SEXP rIndex, SEXP diagC, SEXP nCol, SEXP rhs, SEXP b, SEXP d, SEXP B0, SEXP varE, SEXP varB, SEXP RSS);
