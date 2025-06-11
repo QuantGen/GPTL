d4d9c5ee7fc1d018c7fa0381ed281a6e26bdf44c
Paulino Pérez-Rodríguez <perpdgo@gmail.com>
2025-06-11 12:15:02 -0600

    Delete src/GRAD_DESC.h

--- src/GRAD_DESC.h
+++
@@ -1,6 +0,0 @@
-#define R_NO_REMAP
-
-#include <Rinternals.h>
-
-SEXP GRAD_DESC(SEXP C, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP learning_rate);
-SEXP GRAD_DESC_sparse(SEXP C, SEXP cOffset, SEXP rIndex, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP learning_rate);
