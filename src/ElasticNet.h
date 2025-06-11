c485958a8984a4832d73aeb0b1ac8360c52401a6
Paulino Pérez-Rodríguez <perpdgo@gmail.com>
2025-06-11 12:14:47 -0600

    Delete src/ElasticNet.h

--- src/ElasticNet.h
+++
@@ -1,6 +0,0 @@
-#define R_NO_REMAP
-
-#include <Rinternals.h>
-
-SEXP ElasticNet(SEXP C, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP lambda1, SEXP lambda2, SEXP b0);
-SEXP ElasticNet_sparse(SEXP C, SEXP cOffset, SEXP rIndex, SEXP diagC, SEXP rhs, SEXP b, SEXP nCol, SEXP nIter, SEXP lambda1, SEXP lambda2, SEXP b0);
