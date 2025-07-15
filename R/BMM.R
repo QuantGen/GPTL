460b248885f6a59084112c1303f613819c5a134e
Harold Wu <wuhao12@msu.edu>
2025-07-15 11:34:36 -0400

    Update BMM.R

--- R/BMM.R
+++ R/BMM.R
@@ -10,18 +10,32 @@
 #  To do: sample error variance ; add prior probabilities for the mixtures (right now equivalent to 1/nClasses); priors for the variances
 ##
 
-BMM=function(XX,Xy,my,vy,n,B=matrix(nrow=ncol(XX),ncol=1,0),nIter=150,burnIn=50,thin=5,R2=0.25,
-	        nComp=matrix(ncol(B)),K=1/nComp, df0.E=5,S0.E=vy*(1-R2)*df0.E,df0.b=rep(10,nComp), 
-	        priorProb=rep(1/nComp,nComp),priorCounts=rep(2*nComp,nComp),verbose=TRUE){
-	
+BMM.SS=function(XX, Xy, B, my, vy, n, nIter=150, burnIn=50, thin=5, R2=0.25,
+	        nComp=matrix(ncol(B)), K=1/nComp, df0.E=5, S0.E=vy*(1-R2)*df0.E, df0.b=rep(10,nComp), 
+	        priorProb=rep(1/nComp,nComp), priorCounts=rep(2*nComp,nComp), verbose=TRUE){
+
+ if(!(is(XX,"matrix") | is(XX,"dgCMatrix"))) stop("XX must be a matrix or dgCMatrix\n")
+ if (rownames(XX) != colnames(XX)) stop("Rowname and colname in XX not match\n")
+ snp_list=Reduce(intersect, list(rownames(XX),rownames(Xy),rownames(B)))
+ if (length(snp_list) == 0) stop("No matched SNPs in XX, Xy, and prior\n")
+ XX=XX[snp_list,snp_list]
+ Xy=Xy[snp_list,]
+ B=B[snp_list,]
+
  B=as.matrix(B)
  # nIter=150;burnIn=50;R2=.5;nComp=matrix(ncol(B));df0.E=5;S0.E=vy*R2*df0.E;df0.b=rep(5,nComp);alpha=.1;my=mean(y); vy=var(y); B=cbind(rep(0,p),-1,1)
  p=ncol(XX) 
  b=rowMeans(B)
  d=rep(1,p) # indicator variable for the group
  POST.PROB=matrix(nrow=p,ncol=nComp,0)
-	
- S0.b=c(df0.b)*c(vy)*c(R2*K)/c(sum(diag(XX))/n) # dividing R2/10 assumes that most of the vairance is between components.
+ 
+ # dividing R2/10 assumes that most of the vairance is between components.
+ if(is(XX,"dgCMatrix"))
+ {
+ 	S0.b=c(df0.b)*c(vy)*c(R2*K)/c(sum(as.vector(Matrix::diag(XX)))/n)
+ }else{
+ 	S0.b=c(df0.b)*c(vy)*c(R2*K)/c(sum(diag(XX))/n) 
+ }
  varB=(S0.b/df0.b)
 
  priorProb=priorProb/sum(priorProb)
@@ -33,8 +47,11 @@ BMM=function(XX,Xy,my,vy,n,B=matrix(nrow=ncol(XX),ncol=1,0),nIter=150,burnIn=50,
  postMeanVarB=rep(0,nComp)
  postProb=rep(0,nComp)
 
+ #Need to convert to numeric because if XX is sparse, the result is an object 
+ #of class dgeMatrix
  RSS=vy*(n-1)+crossprod(b,XX)%*%b-2*crossprod(b,Xy)
-	
+ RSS=as.numeric(RSS)
+
  varE=RSS/n
 	
  counts=priorCounts/as.vector(nComp)
@@ -50,11 +67,20 @@ BMM=function(XX,Xy,my,vy,n,B=matrix(nrow=ncol(XX),ncol=1,0),nIter=150,burnIn=50,
  weightPostMeans=1/round((nIter-burnIn)/thin)
 	
  for(i in 1:nIter){        
-	 ## Sampling effects
-	 timeIn=proc.time()[3]
-	  tmp=sample_effects_new(C=XX,rhs=Xy,b=b,d=d,B0=B,varE=varE,varB=varB[d],RSS=RSS)
-          b=tmp[[1]]
-          RSS=tmp[[2]]
+	  ## Sampling effects
+	  timeIn=proc.time()[3]
+	  
+	  if(is(XX,"dgCMatrix"))
+	  {
+	  	#Sparse matrix
+	  	tmp=sample_effects_new_sparse(C=XX,rhs=Xy,b=b,d=d,B0=B,varE=varE,varB=varB[d],RSS=RSS)
+	  }else{
+	  	#Dense matrix
+	  	tmp=sample_effects_new(C=XX,rhs=Xy,b=b,d=d,B0=B,varE=varE,varB=varB[d],RSS=RSS)
+	  }
+	  
+      b=tmp[[1]]
+      RSS=tmp[[2]]
 	 timeEffects=timeEffects+(proc.time()[3]-timeIn)
 	## End of C-code
 	 samplesB[i,]=b
@@ -117,60 +143,21 @@ BMM=function(XX,Xy,my,vy,n,B=matrix(nrow=ncol(XX),ncol=1,0),nIter=150,burnIn=50,
  return(list(b=postMeanB,POST.PROB=POST.PROB,postMeanVarB=postMeanVarB,postProb=postProb,
 	     	samplesVarB=samplesVarB,samplesB=samplesB,samplesVarE=samplesVarE))
 }
-## A function to sample from a Dirichlet
 
-rDirichlet=function(counts){
-    x=rgamma(n=length(counts),shape=counts)
-	return(x/sum(x))
-}
 
-## Two functions to sample integers with pre-specified probabilities
-which.first=function(x){
-	which(x)[1]
-}
-
-sampleComp=function(PROB){
- n=nrow(PROB)
- rSum=rowSums(PROB)
- PROB=apply(FUN=cumsum,X=PROB,MARGIN=1)
- PROB=sweep(PROB,FUN='/',MARGIN=2,STATS=rSum)
- u=runif(n)
- PROB=sweep(PROB,FUN='>',MARGIN=2,STATS=u)
- apply(FUN=which.first,X=PROB,MARGIN=2)
-}
-
-# A Gibbs Sampler for a Bayesian Mixture Model
-# XX=X'X, Xy=X'y, my=mean(y), vy=var(y),n=length(y)
-# Note: we don't include an intercept, so, before computing X'y, X'X, you should center X and y arount their means.
-# B, a matrix with prior estiamtes of marker effects, as many columns as prior estimates. We also suggest including one column
-#     full of zeroes, to allow for just plain shrinkage towards zero, if needed (this is the default value)
-# nIter, burnIn: the number of iterations and burnIn used for the Gibbs Sampler
-# R2: the prior proportion of variance of y explained by the model (this is used to derive hyper-parameters for the prior variances)
-# priorProb a vector of prior probabilities for the mixture compoentns (as many entries as columsn in B), we internally scale it to add up to one
-# priorCounts, the number of counts associated to priorProb, using priorCounts very large (e.g., 1e8) fixes the prior probabilities. The default value is 2*ncol(B)
-#  To do: sample error variance ; add prior probabilities for the mixtures (right now equivalent to 1/nClasses); priors for the variances
-##
-
-BMM2=function(XX,Xy,my,vy,n,B=matrix(nrow=ncol(XX),ncol=1,0),nIter=150,burnIn=50,thin=5,R2=0.25,
+# Stable BMM function before implementing sparse sufficient statistics
+BMM1=function(XX,Xy,my,vy,n,B=matrix(nrow=ncol(XX),ncol=1,0),nIter=150,burnIn=50,thin=5,R2=0.25,
 	        nComp=matrix(ncol(B)),K=1/nComp, df0.E=5,S0.E=vy*(1-R2)*df0.E,df0.b=rep(10,nComp), 
 	        priorProb=rep(1/nComp,nComp),priorCounts=rep(2*nComp,nComp),verbose=TRUE){
-
- if(!(is(XX,"matrix") | is(XX,"dgCMatrix"))) stop("XX must be a matrix or dgCMatrix\n")
- 
+	
  B=as.matrix(B)
  # nIter=150;burnIn=50;R2=.5;nComp=matrix(ncol(B));df0.E=5;S0.E=vy*R2*df0.E;df0.b=rep(5,nComp);alpha=.1;my=mean(y); vy=var(y); B=cbind(rep(0,p),-1,1)
  p=ncol(XX) 
  b=rowMeans(B)
  d=rep(1,p) # indicator variable for the group
  POST.PROB=matrix(nrow=p,ncol=nComp,0)
- 
- # dividing R2/10 assumes that most of the vairance is between components.
- if(is(XX,"dgCMatrix"))
- {
- 	S0.b=c(df0.b)*c(vy)*c(R2*K)/c(sum(as.vector(Matrix::diag(XX)))/n)
- }else{
- 	S0.b=c(df0.b)*c(vy)*c(R2*K)/c(sum(diag(XX))/n) 
- }
+	
+ S0.b=c(df0.b)*c(vy)*c(R2*K)/c(sum(diag(XX))/n) # dividing R2/10 assumes that most of the vairance is between components.
  varB=(S0.b/df0.b)
 
  priorProb=priorProb/sum(priorProb)
@@ -182,11 +169,8 @@ BMM2=function(XX,Xy,my,vy,n,B=matrix(nrow=ncol(XX),ncol=1,0),nIter=150,burnIn=50
  postMeanVarB=rep(0,nComp)
  postProb=rep(0,nComp)
 
- #Need to convert to numeric because if XX is sparse, the result is an object 
- #of class dgeMatrix
  RSS=vy*(n-1)+crossprod(b,XX)%*%b-2*crossprod(b,Xy)
- RSS=as.numeric(RSS)
-
+	
  varE=RSS/n
 	
  counts=priorCounts/as.vector(nComp)
@@ -202,20 +186,11 @@ BMM2=function(XX,Xy,my,vy,n,B=matrix(nrow=ncol(XX),ncol=1,0),nIter=150,burnIn=50
  weightPostMeans=1/round((nIter-burnIn)/thin)
 	
  for(i in 1:nIter){        
-	  ## Sampling effects
-	  timeIn=proc.time()[3]
-	  
-	  if(is(XX,"dgCMatrix"))
-	  {
-	  	#Sparse matrix
-	  	tmp=sample_effects_new_sparse(C=XX,rhs=Xy,b=b,d=d,B0=B,varE=varE,varB=varB[d],RSS=RSS)
-	  }else{
-	  	#Dense matrix
-	  	tmp=sample_effects_new(C=XX,rhs=Xy,b=b,d=d,B0=B,varE=varE,varB=varB[d],RSS=RSS)
-	  }
-	  
-      b=tmp[[1]]
-      RSS=tmp[[2]]
+	 ## Sampling effects
+	 timeIn=proc.time()[3]
+	  tmp=sample_effects_new(C=XX,rhs=Xy,b=b,d=d,B0=B,varE=varE,varB=varB[d],RSS=RSS)
+          b=tmp[[1]]
+          RSS=tmp[[2]]
 	 timeEffects=timeEffects+(proc.time()[3]-timeIn)
 	## End of C-code
 	 samplesB[i,]=b
