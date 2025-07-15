8d7f1fdeab870272334d4f4b87bb0fec79cf26fc
Harold Wu <wuhao12@msu.edu>
2025-07-15 11:59:55 -0400

    Update PR.R

--- R/PR.R
+++ R/PR.R
@@ -3,7 +3,7 @@ PR.SS<- function(XX, Xy, b, lambda=NULL, nLambda=30, alpha=0, conv_threshold=1e-
 
   if(!(is(XX,"matrix") | is(XX,"dgCMatrix"))) stop("XX must be a matrix or dgCMatrix\n")
   
-  if (rownames(XX) != colnames(XX)) stop("Rowname and colname in XX not match\n")
+  if (!all(rownames(XX) == colnames(XX))) stop("Rowname and colname in XX not match\n")
   
   snp_list=Reduce(intersect, list(rownames(XX),rownames(Xy),rownames(b)))
   if (length(snp_list) == 0) stop("No matched SNPs in XX, Xy, and prior\n")
@@ -85,7 +85,7 @@ PR.SS<- function(XX, Xy, b, lambda=NULL, nLambda=30, alpha=0, conv_threshold=1e-
 PR<- function(ld, gwas, b, lambda=NULL, nLambda=30, alpha=0, conv_threshold=1e-4, maxIter=500, returnPath=TRUE) {
   #alpha=0 -> Ridge; alpha=1 -> Lasso
 
-  if (rownames(ld) != colnames(ld)) stop("Rowname and colname in LD not match\n")
+  if (!all(rownames(XX) == colnames(XX))) stop("Rowname and colname in XX not match\n")
 
   if (!all(c('id', 'beta', 'se', 'n', 'allele_freq') %in% colnames(gwas))) stop("Must provide GWAS results that consist of columns: id (variant IDs), beta (variant effects), se (variant standard errors), n (sample sizes for GWAS), allele_freq (variant allele frequency)\n")
  
