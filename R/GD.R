bca76e0ab8649d4df7841c53e6a8dc3a1e65c153
Harold Wu <wuhao12@msu.edu>
2025-10-28 11:38:23 -0400

    Update GD.R

--- R/GD.R
+++ R/GD.R
@@ -1,56 +1,46 @@
 GD<- function(XX, Xy, b, nIter=10, learning_rate=1/50, lambda=0, lambda0=1, returnPath=FALSE, verbose=TRUE){
 
-    if(!(is(XX,"matrix") | is(XX,"dgCMatrix"))) stop("XX must be a matrix or dgCMatrix\n")
-
-    if(is.null(rownames(XX)) | is.null(colnames(XX))){
-        stop('XX must have variant IDs as row and column names\n')
-    }
+    if(!(is(XX,"matrix") | is(XX,"dgCMatrix") | (nrow(XX)==ncol(XX)))) stop("XX must be a square matrix or dgCMatrix\n")
+    if(!(is(Xy,"vector") | is(Xy,"matrix") | is(Xy,"data.frame"))) stop("Xy must be in one of these formats: vector, matrix or data.frame with single column\n")
     
-    if (!all(rownames(XX) == colnames(XX))) stop("Row and column names in XX not match\n")
-
-    if (is.vector(Xy)) {
-        if (is.null(names(Xy))) {
-            stop("Xy must have variant IDs as names\n")
-        }
-    } else if (is.matrix(Xy) | is.data.frame(Xy)) {
-        if (is.null(rownames(Xy))) {
-            stop("Xy must have variant IDs as row names\n")
-        }
-        XyName=rownames(Xy)
-        Xy=as.vector(as.matrix(Xy))
-        names(Xy)=XyName
-    } else {
-        stop("Xy must be in one of these formats: vector, matrix, data.frame\n")
+    if(is.null(b)){
+        b=rep(0, nrow(XX))
+        names(b)=rownames(XX)
     }
 
-    if (is.vector(b)) {
-        if (is.null(names(b))) {
-            stop("The prior estimates vector (b) must have variant IDs as names\n")
-        }
-    } else if (is.matrix(b) | is.data.frame(b)) {
-        if (is.null(rownames(b))) {
-            stop("The prior estimates matrix (b) must have variant IDs as row names\n")
-        }
-        bName=rownames(b)
-        b=as.vector(as.matrix(b))
-        names(b)=bName
-    } else {
-        stop("b must be in one of these formats: vector, matrix, data.frame\n")
-    }
-    
-    snp_list=Reduce(intersect, list(rownames(XX),names(Xy),names(b)))
+    if(!(is(b,"vector") | is(b,"matrix") | is(b,"data.frame"))) stop("The prior estimates (b) must be in one of these formats: vector, matrix or data.frame with single column\n")
 
+    Xy=as.matrix(Xy)
+    b=as.matrix(b)
+
+    nameWarningFlag=0
+    if(is.null(rownames(XX)) | is.null(colnames(XX)) | is.null(rownames(Xy)) | is.null(rownames(b))){
+        warning('Variant IDs are missing in one or more of inputs: XX, Xy, or b\n')
+        nameWarningFlag=1
+    }
+
+    snp_list=Reduce(intersect, list(rownames(XX),rownames(Xy),rownames(b)))
     if (length(snp_list) == 0){ 
-        stop("No matched variants in XX, Xy, and prior\n")
-    }else{
+        warning('Variant IDs are not matching between inputs: XX, Xy, or b\n')
+        nameWarningFlag=1
+    }
+
+    if (nrow(XX)!=nrow(Xy) | nrow(XX)!=nrow(b)){
+        stop('Distinct number of variants detected in inputs: XX, Xy, and b, while variant IDs are missing in one or more of inputs: XX, Xy, or b\n')
+    }
+
+    if (nameWarningFlag==0){
+        XX=XX[snp_list,snp_list,drop = FALSE]
+        Xy=Xy[snp_list,,drop = FALSE]
+        b=b[snp_list,,drop = FALSE]
         if(verbose){
-            message(' There were ',length(snp_list), ' variants in common between XX, Xy, and the prior.\n')
+            message(length(snp_list), ' variants in common between XX, Xy, and b are retained\n')
+        }
+    } else {
+        if(verbose){
+            message(nrow(XX), ' variants are retained\n')
         }
     }
-    
-    XX=XX[snp_list,snp_list,drop = FALSE]
-    Xy=Xy[snp_list]
-    b=b[snp_list]
 
     p=nrow(XX)
     b0=rep(0,p)
