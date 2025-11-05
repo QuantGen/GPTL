654feb3f1e0e3969d0f812eb2df967b55c8cad69
Gustavo de los Campos <gdeloscampos@gmail.com>
2025-11-05 08:31:52 -0500

    Update GD.CV.R

--- R/GD.CV.R
+++ R/GD.CV.R
@@ -1,5 +1,6 @@
 
-GD.CV=function(X,y,nTst=round(nrow(X)*0.2),nRep=10,nIter=100,seed=12345,b=rep(0,ncol(X)),...){
+GD.CV=function(X,y,nTst=round(nrow(X)*0.2),nRep=10,nIter=100,seed=NULL,b=rep(0,ncol(X)),...){
+	if(!is.null(seed)){ set.seed(seed) }
 
 	COR=matrix(nrow=nRep,ncol=nIter)
 	set.seed(seed)
