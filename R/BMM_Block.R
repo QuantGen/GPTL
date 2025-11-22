8c3c81d84886f40868b58648f2446a1e8f5a3fcf
Harold Wu <wuhao12@msu.edu>
2025-11-22 16:53:58 -0500

    Update BMM_Block.R

--- R/BMM_Block.R
+++ R/BMM_Block.R
@@ -1,4 +1,4 @@
-BMM_Block=function(XX, Xy, B, my, vy, n, nIter=1200, burnIn=200, thin=5, R2=0.25,
+BMM_Block=function(XX, Xy, B, my=0, vy=1, n, nIter=1200, burnIn=200, thin=5, R2=0.25,
 	        nComp=matrix(ncol(B)), K=1/nComp, df0.E=5, S0.E=vy*(1-R2)*df0.E, df0.b=rep(10,nComp), 
 	        priorProb=rep(1/nComp,nComp), priorCounts=rep(2*nComp,nComp), 
 			fixVarE=FALSE, fixVarB=rep(FALSE,ncol(B)), verbose=TRUE, ...) {
