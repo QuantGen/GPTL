62ccb5d8e480a4ab0ea3ca8a842d659df30ca453
Gustavo de los Campos <gdeloscampos@gmail.com>
2026-09-10 17:49:52 -0400

    Refactor GDXy2 function with early stopping
    Updated GDXy2 function to include early stopping feature and adjusted minChange default value.

--- R/GDXy2.R
+++ R/GDXy2.R
@@ -1,8 +1,6 @@
 # A function to perform Grad. Desc. that takes X and y, instead of XX and Xy
 
-GDXy2<-function(X,y,b=NULL,learningRate=1/100,nIter=100,minChange=Inf){
-   
-   earlyStop=(minChange<Inf)
+GDXy2<-function(X,y,b=NULL,learningRate=1/100,nIter=100,minChange=1,earlyStop=FALSE){
 
    p=ncol(X)
    X=scale(X,center=TRUE,scale=FALSE)
@@ -16,17 +14,21 @@ GDXy2<-function(X,y,b=NULL,learningRate=1/100,nIter=100,minChange=Inf){
 
    learningRate=learningRate/mean(x2)
 
+   if(earlyStop){
+      minChange=learningRate*minChange
+   }
+
    e=y-X%*%b
    
    B=matrix(nrow=p,ncol=nIter+1,NA)
    B[,1]=b
 
-   if(ES){ RSS=rep(NA_numeric_,nIter+1) }
+   if(earlyStop){ RSS=rep(NA_numeric_,nIter+1) }
    RSS[1]=sum(e^2)
    for(i in 2:(nIter+1)){
         .Call("GRAD_DESC_Xy", X, x2, b, e, nrow(X), ncol(X), 1, learningRate)
         B[,i]=b
-        if(ES){
+        if(earlyStop){
             RSS[i]=sum((y-X%*%b)^2)
             if( (1-(RSS[i]/RSS[i-1]))<minChange){ 
                 B=B[,1:i]
