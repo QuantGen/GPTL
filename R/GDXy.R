64b3eeb9393b4a16796d25540d7089cf5f20d6e9
Gustavo de los Campos <gdeloscampos@gmail.com>
2026-09-11 10:04:10 -0400

    Add early stopping option to GDXy function

--- R/GDXy.R
+++ R/GDXy.R
@@ -1,14 +1,18 @@
 # A function to perform Grad. Desc. that takes X and y, instead of XX and Xy
 
-GDXy=function(X,y,centerX=TRUE,scaleX=FALSE,...){
+GDXy=function(X,y,centerX=TRUE,scaleX=FALSE,earlyStop=FALSE,pctChangeRSS=0.02,...){
 
     X=scale(X,center=centerX,scale=scaleX)
     y=scale(y,center=TRUE)
 
     XX=crossprod(X)
     Xy=crossprod(X,y)
-
-    B=GD(XX=XX,Xy=Xy,...)
-
+    if(earlyStop){
+        B=GD.Auto.RSS(XX=XX,Xy=Xy,pctChangeRSS=pctChangeRSS,...)
+    }else{
+        B=GD(XX=XX,Xy=Xy,...)
+    }
     return(B)
 }
+
+#GD.Auto.RSS<- function(XX, Xy, b=NULL, maxIter=10, learningRate=1/50, lambda=0, verbose=TRUE,pctChangeRSS=learningRate*2)
