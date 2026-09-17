f466e3a009bd97227cc7bba49b28ed4e2ce5d3bc
Gustavo de los Campos <gdeloscampos@gmail.com>
2026-09-16 21:31:57 -0400

    Replace corBootstrap with cor for correlation calculation

--- R/GD.ES.CV.SS.R
+++ R/GD.ES.CV.SS.R
@@ -73,7 +73,7 @@ GD.ES.CV.SS<- function( X,y, trn,  centerX=TRUE,scaleX=FALSE, b=NULL, maxIter=30
    
     B=matrix(nrow=p,ncol=maxIter+1)
     Cor=numeric(maxIter+1)
-    Cor[1]=corBootstrap(X_tst%*%b, y_tst,100)#cor(X_tst%*%b, y_tst)
+    Cor[1]=cor(X_tst%*%b, y_tst)#corBootstrap(X_tst%*%b, y_tst,100)#
     
     diag(XX)=diag(XX)+(lambda)
     LR=learningRate/mean(diag(XX))
@@ -82,7 +82,7 @@ GD.ES.CV.SS<- function( X,y, trn,  centerX=TRUE,scaleX=FALSE, b=NULL, maxIter=30
     
     for(i in 2:ncol(B)){
         B[,i]=.Call("GRAD_DESC",XX, Xy, B[,i-1],p, 1, LR)
-        Cor[i]=corBootstrap(X_tst%*%B[,i], y_tst,100)#cor(X_tst%*%B[,i], y_tst)
+        Cor[i]=cor(X_tst%*%B[,i], y_tst)#corBootstrap(X_tst%*%B[,i], y_tst,100)#
         if (Cor[i]<Cor[i-1]) {break}
     }
         
